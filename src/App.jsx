3e90985cc3265de104a65f6958aed40598e49474
chen <23054115+cwkang1998@users.noreply.github.com>
2021-12-25 03:39:56 +0800

    feat: Add connect button for metamask

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+  const {connectWallet, address, error, provider} = useWeb3();
+  console.log("Address:", address);
+
+  if(!address){
+    return (
+      <div className="landing">
+        <h1>Welcome to DkDAO</h1>
+        <button onClick={() => {connectWallet("injected")}}>Connect your wallet</button>
+      </div>
+    );
+  }
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>Wallet connected!</h1>
     </div>
-  );
+  )
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.jsx";
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
+
+// Here are the chains you want to support
+// Rinkenby is what we use, which has chainId = 4
+const supportedChainIds = [4];
+
+// Connectors are essentailly wallets,
+// And metamask is reffered as injected.
+const connectors = {
+  injected: {},
+};
 
 // Render the App component to the DOM
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThirdwebWeb3Provider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <App />
+    </ThirdwebWeb3Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
